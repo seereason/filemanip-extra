84682e6f09e4b6770395016e4f799bc1a7f0b740
David Fox <dsf@seereason.com>
2015-04-04 10:40:35 -0700

    Add a WithProcAndSys type to mark computations that require /proc and /sys

--- System/FilePath/Extra4.hs
+++ System/FilePath/Extra4.hs
@@ -1,14 +1,17 @@
 -- | Was in Debian.Repo.MonadOS
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module System.FilePath.Extra4
     ( withMount
+    , WithProcAndSys
     , withProcAndSys
     , withTmp
     ) where
 
+import Control.Applicative (Applicative)
 import Control.Exception (catch)
 import Control.Monad.Catch (bracket, MonadCatch, MonadMask)
-import Control.Monad.Trans (liftIO, MonadIO)
+import Control.Monad.Trans (MonadTrans, lift, liftIO, MonadIO)
 -- import Control.Monad.Trans.Except ({- ExceptT instances -})
 import Data.ByteString.Lazy as L (ByteString, empty)
 import GHC.IO.Exception (IOErrorType(OtherError))
@@ -30,24 +33,33 @@ readProcess p input = do
 -- | Do an IO task with a file system remounted using mount --bind.
 -- This was written to set up a build environment.
 withMount :: forall m c. (MonadIO m, MonadCatch m, MonadMask m) => FilePath -> FilePath -> m c -> m c
-withMount src dest task =
+withMount directory mountpoint task =
     bracket pre (\ _ -> post) (\ _ -> task)
     where
-      mount = proc "mount" ["--bind", src, dest]
-      umount = proc "umount" [dest]
-      umountLazy = proc "umount" ["-l", dest]
+      mount = proc "mount" ["--bind", directory, mountpoint]
+      umount = proc "umount" [mountpoint]
+      umountLazy = proc "umount" ["-l", mountpoint]
       pre :: m L.ByteString
-      pre = liftIO $ do createDirectoryIfMissing True dest
+      pre = liftIO $ do createDirectoryIfMissing True mountpoint
                         readProcess mount L.empty
       post :: m L.ByteString
       post = liftIO $ do readProcess umount L.empty
                            `catch` (\ (e :: IOError) ->
-                                        do hPutStrLn stderr ("Exception unmounting " ++ dest ++ ", trying -l: " ++ show e)
+                                        do hPutStrLn stderr ("Exception unmounting " ++ mountpoint ++ ", trying -l: " ++ show e)
                                            readProcess umountLazy L.empty)
 
-withProcAndSys :: forall m c. (MonadIO m, MonadCatch m, MonadMask m) => FilePath -> m c -> m c
+newtype WithProcAndSys m a = WithProcAndSys { runWithProcAndSys :: m a } deriving (Functor, Monad, Applicative)
+
+instance MonadTrans WithProcAndSys where
+    lift = WithProcAndSys
+
+instance MonadIO m => MonadIO (WithProcAndSys m) where
+    liftIO task = WithProcAndSys (liftIO task)
+
+withProcAndSys :: forall m c. (MonadIO m, MonadCatch m, MonadMask m) => FilePath -> WithProcAndSys m c -> m c
+withProcAndSys "/" task = runWithProcAndSys task -- If root is / these should already be mounted
 withProcAndSys root task =
-    withMount "/proc" (root </> "proc") $ withMount "/sys" (root </> "sys") $ task
+    withMount "/proc" (root </> "proc") $ withMount "/sys" (root </> "sys") $ runWithProcAndSys task
 
 -- | Do an IO task with /tmp remounted.  This could be used
 -- to share /tmp with a build root.
